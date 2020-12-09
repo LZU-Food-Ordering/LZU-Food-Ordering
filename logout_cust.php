a7a1d7d92a6410af0a392d044658bcdb208b996d
somsura <2360349548@qq.com>
2020-12-09 21:08:54 +0800

    Polish database structure
    Signed-off-by: somsura <2360349548@qq.com>

---
+++ logout_cust.php
@@ -0,0 +1,22 @@
+<?php
+
+// include function files for this application
+require_once('food_sc_fns.php');
+session_start();
+$old_user = $_SESSION['cust_user'];  // store  to test if they *were* logged in
+unset($_SESSION['cust_user']);
+session_destroy();
+
+// start output html
+do_html_header("Logging Out");
+
+if (!empty($old_user)) {
+  echo "<p>Logged out.</p>";
+  do_html_url("cust_login.php", "Login");
+} else {
+  // if they weren't logged in but came to this page somehow
+  echo "<p>You were not logged in, and so have not been logged out.</p>";
+  do_html_url("cust_login.php", "Login");
+}
+
+do_html_footer();
